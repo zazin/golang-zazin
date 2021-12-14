1c2b1e1fc17a676ea30ba1fa42c69d03d3e58562
nur zazin <nurza.cool@gmail.com>
2021-12-14 09:29:37 +0700

    add port

--- main.go
+++ main.go
@@ -6,20 +6,26 @@ import (
 	"golang-zazin/internal/delivery/http/user_handler"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	r := mux.NewRouter()
 	initHandler(r)
-	addr := "127.0.0.1:8000"
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         addr,
+		Addr:         port,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	log.Println(fmt.Sprintf("server running at : %s", addr))
+	log.Println(fmt.Sprintf("server running at : %s", port))
 	log.Fatal(srv.ListenAndServe())
 }
 
